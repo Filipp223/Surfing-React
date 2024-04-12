03b2b16a80507e5a30067b241cdb6fc31b2c7037
Filipp223 <filippfrolov@icloud.com>
2024-04-12 18:20:34 +0200

    Integrated images

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,3 +1,18 @@
+import images1p1 from "./images/s1p1.png";
+import images1p2 from "./images/s1p2.png";
+import images1p3 from "./images/s1p3.png";
+import images1p4 from "./images/s1p4.png";
+import images1p5 from "./images/s1p5.png";
+import images1p6 from "./images/s1p6.png";
+import images1p7 from "./images/s1p7.png";
+import images1p8 from "./images/s1p8.png";
+import images1p9 from "./images/s1p9.png";
+import images2p1 from "./images/s2p1.png";
+import images2p2 from "./images/s2p2.png";
+import images2p3 from "./images/s2p3.png";
+import images2p4 from "./images/s2p4.png";
+import images2p5 from "./images/s2p5.png";
+import images2p6 from "./images/s2p6.png";
 import "./css/cards/cards.css";
 import "./css/speech/speech.css";
 import "./css/gallery/gallery.css";
@@ -7,15 +22,15 @@ function Main() {
         <main>
             <section className="gallery container">
                 <div className="gallery__content">
-                    <img src="images/s1p1.png" alt="Surfer on a wave" className="gallery__item"/>
-                    <img src="images/s1p2.png" alt="Dog on the board" className="gallery__item"/>
-                    <img src="images/s1p3.png" alt="Surfer on a wave" className="gallery__item"/>
-                    <img src="images/s1p4.png" alt="Surfer on a wave" className="gallery__item"/>
-                    <img src="images/s1p5.png" alt="Surfboards" className="gallery__item"/>
-                    <img src="images/s1p6.png" alt="Surfer on a wave" className="gallery__item"/>
-                    <img src="images/s1p7.png" alt="Surfer on a wave" className="gallery__item"/>
-                    <img src="images/s1p8.png" alt="Surfer on the beach" className="gallery__item"/>
-                    <img src="images/s1p9.png" alt="Surfer on the beach" className="gallery__item"/>
+                    <img src={images1p1} alt="Surfer on a wave" className="gallery__item"/>
+                    <img src={images1p2} alt="Dog on the board" className="gallery__item"/>
+                    <img src={images1p3} alt="Surfer on a wave" className="gallery__item"/>
+                    <img src={images1p4} alt="Surfer on a wave" className="gallery__item"/>
+                    <img src={images1p5} alt="Surfboards" className="gallery__item"/>
+                    <img src={images1p6} alt="Surfer on a wave" className="gallery__item"/>
+                    <img src={images1p7} alt="Surfer on a wave" className="gallery__item"/>
+                    <img src={images1p8} alt="Surfer on the beach" className="gallery__item"/>
+                    <img src={images1p9} alt="Surfer on the beach" className="gallery__item"/>
                 </div>
             </section>
 
@@ -23,27 +38,27 @@ function Main() {
             <section className="cards container">
                 <div className="cards__grid">
                     <div className="cards__item card1">
-                        <img src="images/s2p1.png" alt="A picture of an airplane flying away"/>
+                        <img src={images2p1} alt="A picture of an airplane flying away"/>
                         <p>Travel</p>
                     </div>
                     <div className="cards__item card2">
-                        <img src="images/s2p2.png" alt="Surfboards picture"/>
+                        <img src={images2p2} alt="Surfboards picture"/>
                         <p>Buy with us</p>
                     </div>
                     <div className="cards__item card3">
-                        <img src="images/s2p3.png" alt="A picture of the training process"/>
+                        <img src={images2p3} alt="A picture of the training process"/>
                         <p>Work out with a trainer</p>
                     </div>
                     <div className="cards__item card4">
-                        <img src="images/s2p4.png" alt="Surfer on a wave"/>
+                        <img src={images2p4} alt="Surfer on a wave"/>
                         <p>Enjoy</p>
                     </div>
                     <div className="cards__item card5">
-                        <img src="images/s2p5.png" alt="Beach image"/>
+                        <img src={images2p5} alt="Beach image"/>
                         <p>The best beaches</p>
                     </div>
                     <div className="cards__item card6">
-                        <img src="images/s2p6.png" alt="Surfer conquering a wave"/>
+                        <img src={images2p6} alt="Surfer conquering a wave"/>
                         <p>Improve your skills</p>
                     </div>
                 </div>
