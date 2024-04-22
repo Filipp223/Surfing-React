9c4d72ac641a6ec264e72fc73251ebd60f65e93e
Filipp223 <filippfrolov@icloud.com>
2024-04-22 17:08:14 +0200

    Added Cards and implemented into Main.jsx

---
+++ src/Cards.jsx
@@ -0,0 +1,41 @@
+import images2p1 from "./images/s2p1.png";
+import images2p2 from "./images/s2p2.png";
+import images2p3 from "./images/s2p3.png";
+import images2p4 from "./images/s2p4.png";
+import images2p5 from "./images/s2p5.png";
+import images2p6 from "./images/s2p6.png";
+
+function Cards() {
+    return (
+        <section className="cards container">
+                <div className="cards__grid">
+                    <div className="cards__item card1">
+                        <img src={images2p1} alt="A picture of an airplane flying away"/>
+                        <p>Travel</p>
+                    </div>
+                    <div className="cards__item card2">
+                        <img src={images2p2} alt="Surfboards picture"/>
+                        <p>Buy with us</p>
+                    </div>
+                    <div className="cards__item card3">
+                        <img src={images2p3} alt="A picture of the training process"/>
+                        <p>Work out with a trainer</p>
+                    </div>
+                    <div className="cards__item card4">
+                        <img src={images2p4} alt="Surfer on a wave"/>
+                        <p>Enjoy</p>
+                    </div>
+                    <div className="cards__item card5">
+                        <img src={images2p5} alt="Beach image"/>
+                        <p>The best beaches</p>
+                    </div>
+                    <div className="cards__item card6">
+                        <img src={images2p6} alt="Surfer conquering a wave"/>
+                        <p>Improve your skills</p>
+                    </div>
+                </div>
+        </section>
+    )
+}
+
+export default Cards;
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,5 @@
 import Gallery from "./Gallery.jsx";
-import images2p1 from "./images/s2p1.png";
-import images2p2 from "./images/s2p2.png";
-import images2p3 from "./images/s2p3.png";
-import images2p4 from "./images/s2p4.png";
-import images2p5 from "./images/s2p5.png";
-import images2p6 from "./images/s2p6.png";
+import Cards from "./Cards.jsx"
 import "./css/cards/cards.css";
 import "./css/speech/speech.css";
 import "./css/gallery/gallery.css";
@@ -14,35 +9,7 @@ function Main() {
         <main>
             <Gallery/>
 
-            <section className="cards container">
-                <div className="cards__grid">
-                    <div className="cards__item card1">
-                        <img src={images2p1} alt="A picture of an airplane flying away"/>
-                        <p>Travel</p>
-                    </div>
-                    <div className="cards__item card2">
-                        <img src={images2p2} alt="Surfboards picture"/>
-                        <p>Buy with us</p>
-                    </div>
-                    <div className="cards__item card3">
-                        <img src={images2p3} alt="A picture of the training process"/>
-                        <p>Work out with a trainer</p>
-                    </div>
-                    <div className="cards__item card4">
-                        <img src={images2p4} alt="Surfer on a wave"/>
-                        <p>Enjoy</p>
-                    </div>
-                    <div className="cards__item card5">
-                        <img src={images2p5} alt="Beach image"/>
-                        <p>The best beaches</p>
-                    </div>
-                    <div className="cards__item card6">
-                        <img src={images2p6} alt="Surfer conquering a wave"/>
-                        <p>Improve your skills</p>
-                    </div>
-                </div>
-            </section>
-
+            <Cards/>
 
             <section className="speech container">
                 <div className="speech__content">
